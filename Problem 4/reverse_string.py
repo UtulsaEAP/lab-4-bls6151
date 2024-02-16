83cbb6deddda85e9265d630ccf2d05bb772da146
Braden Stanfield <bls6151@utulsa.edu>
2024-02-16 15:46:43 -0600

    prob 4!

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -8,7 +8,14 @@ Lab Time: Fri 3
 def reverse_string():
     # YOUR CODE HERE
     word = input()
-    while word != "done":
+    p = 1
+    if word == "done":
+        p = 0
+    if word == "d":
+        p = 0
+    if word == "Done":
+        p = 0
+    while p == 1:
         
         drow = ""
         t = 1
@@ -17,6 +24,12 @@ def reverse_string():
             t += 1
         print(drow)
         word = input()
+        if word == "done":
+            p = 0
+        if word == "d":
+            p = 0
+        if word == "Done":
+             p = 0
 
 if __name__ == "__main__":
     reverse_string()
