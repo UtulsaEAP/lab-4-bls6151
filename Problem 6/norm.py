8d2f22d0e55e2b6f739ae001f2f6dc48ef9c7902
Braden Stanfield <bls6151@utulsa.edu>
2024-02-22 11:29:13 -0600

    Problem 6

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -9,12 +9,21 @@ def norm():
     # Write your code here
     t = int(input())
     r = 0
-    list = [t]
+    list = []
+    
     while r < t:
         num = input()
-        list[r] = num
+        list.append(num)
         r += 1
-    print(list)
+    lar = float(list[0])
+    for i in range (1,t):
+        if float(list[i]) > lar:
+            lar = float(list[i])
+    for k in range(0,t):
+        list[k] = float(list[k]) / float(lar)
+
+    for j in range (0,t):
+        print(f'{float(list[j]):.2f}')
 
 if __name__ == "__main__":
     norm()
